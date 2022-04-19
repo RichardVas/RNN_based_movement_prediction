e42f56cd019c961f40eb9b42eed2161b50ce6b38
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2022-04-19 02:08:14 +0200

    remove redundant/garbagecode

--- tracker.py
+++ tracker.py
@@ -12,18 +12,15 @@ pred = Predictor()
 
 class EuclideanTracker:
     def __init__(self):
-        # Store the center positions of the objects
+        #needed for euclidean
         self.center_points = {}
-
+        #redundant
         self.everyCenters = {}
-
-        # Keep the count of the IDs
-        # each time a new object id detected, the count will increase by one
+        #id assignment
         self.id_count = 0
-        self.objtoTrack = []
-        #dict that stores obj and their pos based on ids a BAL FELSO SARKAT TAROLJA EL
+        #overall storage
         self.detectedOBJ = defaultdict(list)
-
+        #easier to find colors
         self.uniqueIDSET = defaultdict(tuple)
 
     def genRandomGreen(self):
@@ -33,7 +30,7 @@ class EuclideanTracker:
         color = (redval, greenval, blueval)
         
         return color
-
+    #save pickle and humanly readable logs
     def printobjs(self):
 
         with open('dict1.csv','w') as csv_file:
@@ -55,33 +52,30 @@ class EuclideanTracker:
             return cx,cy
 
     def update(self, objects_rect):
-        # Objects boxes and ids
+        #tracked objs with ids
         objects_bbs_ids = []
         unique_ids = []
 
         actualID_set = set()
-        # Get center point of new object
+
         for rect in objects_rect:
             x, y, w, h = rect
-          #  cx = (x + x + w) // 2
-          #  cy = (y + y + h) // 2
             cx, cy = self.calCenterPoint(x,y,w,h)
-            # Find out if that object was detected already
+
             same_object_detected = False
 
             
 
             for id, pt in self.center_points.items():
                 dist = math.hypot(cx - pt[0], cy - pt[1])
-# ha felveszek egy új objektumok, akkor az id után appendeljünk egy random zöldzerű színt
-# nem jo mert ha ugyan az az elem akkor felvesz mindig új értéket
 
+                #found the same ojb
                 if dist <= 25:
                     self.center_points[id] = (cx, cy)
 
                     self.everyCenters[id] = [cx,cy]
                     #print(self.center_points)
-                    currentobj = self.detectedOBJ[id]
+                   # currentobj = self.detectedOBJ[id]
 
                     objects_bbs_ids.append([x, y, w, h, id])
                     self.detectedOBJ[id].append([x,y,w,h])
@@ -90,7 +84,7 @@ class EuclideanTracker:
                     actualID_set.add(id)
                     break
 
-            # New object is detected we assign the ID to that object
+            #new object
             if same_object_detected is False:
                 self.center_points[self.id_count] = (cx, cy)
                 self.everyCenters[self.id_count] = (cx,cy)
@@ -105,7 +99,7 @@ class EuclideanTracker:
                 self.uniqueIDSET[self.id_count] = self.genRandomGreen()
                 actualID_set.add(self.id_count)
 
-        # Clean the dictionary by center points to remove IDS not used anymore
+       #clean center dict from ununsed ids
         new_center_points = {}
         for obj_bb_id in objects_bbs_ids:
             _, _, _, _, object_id = obj_bb_id
@@ -121,7 +115,6 @@ class EuclideanTracker:
  #           if key not in actualID_set:
  #               del self.detectedOBJ[key]
 
-        # Update dictionary with IDs not used removed
         self.center_points = new_center_points.copy()
 
         return objects_bbs_ids, unique_ids
